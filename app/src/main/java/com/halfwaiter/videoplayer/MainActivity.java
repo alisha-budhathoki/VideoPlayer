d966ef007899d9db6ee70e604a9e379c01ceab15
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-11-25 08:09:16 +0545

    added video view on screen

--- app/src/main/java/com/halfwaiter/videoplayer/MainActivity.java
+++ app/src/main/java/com/halfwaiter/videoplayer/MainActivity.java
@@ -2,13 +2,22 @@ package com.halfwaiter.videoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String videoPath = "android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.nextt_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
     }
 }
